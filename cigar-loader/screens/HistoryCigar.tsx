0c517271536eaf10576279e06207c521f8524ac4
johnniePm <johnnie.jpm@gmail.com>
2024-04-07 12:49:41 +0200

    last

--- cigar-loader/screens/HistoryCigar.tsx
+++ cigar-loader/screens/HistoryCigar.tsx
@@ -1,5 +1,15 @@
+import { View } from "react-native"
+import { ScrollView } from "react-native-gesture-handler"
+import { useTheme, Surface, Text, TouchableRipple } from "react-native-paper"
+
 export default function  HistoryCigar(){
+    const theme = useTheme()
+
     return(
-        <></>
+        <ScrollView>
+            <TouchableRipple   >
+<Text>Hello</Text>
+            </TouchableRipple>
+        </ScrollView>
     )
 }
